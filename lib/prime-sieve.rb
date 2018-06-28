b01a3b6646a177a02c8b833bfb17b7aed12017f3
Brandon Betts and Rita Bennett-Chew <rita@>
2018-06-28 13:33:40 -0700

    finish up prime numbers code

--- lib/prime-sieve.rb
+++ lib/prime-sieve.rb
@@ -1,18 +1,15 @@
 class Int
-
   def initialize(number)
     @number = number
-    @new_array = (2..@number).to_a
+    @number_array = (2..@number).to_a
   end
-
   def show_array
-    @new_array
+    @number_array
   end
-
   def find_primes
-    prime_array = @new_array
-    prime_array.reject { |n| (n > 2 && n % 2 == 0) }
-    prime_array.reject { |n| (n > 3 && n % 3 == 0) }
-    
+    @number_array.each do |n|
+      @number_array = @number_array.reject { |i| (i > n && i % n == 0) }
+    end
+    @number_array
   end
 end
